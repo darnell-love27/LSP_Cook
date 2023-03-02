34c1c4d62e8c4af1b103a930bfba71b6e6615aeb
darnell-love27 <101828196+darnell-love27@users.noreply.github.com>
2023-03-02 17:04:55 -0500

    Created max value instance.

--- lsp-spring-2023/src/org/howard/edu/hw4/IntegerSet.java
+++ lsp-spring-2023/src/org/howard/edu/hw4/IntegerSet.java
@@ -1,10 +1,39 @@
+// Name: Darnell Love
 package org.howard.edu.hw4;
-
+import java.util.*;
 public class IntegerSet {
+	
+	private List<Integer> new_set;
+	
+	//Default Constructor as ArrayList
+	public IntegerSet(){
+		new_set = new ArrayList<Integer>();
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public class IntegerSetException extends Exception{
+		public IntegerSetException(String error) {
+			super(error);
+		}
 
+	}
+	
+	//List used for set of Integers
+	public List<Integer> find_set() {
+		return new_set;
+	}
+	
+	//Clears internal representation of set
+	public void of_set(List<Integer> new_set) {
+		new_set.clear();
+	}
+	
+	//Returns length of set
+	public int length() {
+		return new_set.size();
+	}
+	//
+	public boolean equals(IntegerSet set) {
+		
 	}
 
 }
