a566d65e97ccbaf70f288d63720724f6f498d7cb
darnell-love27 <101828196+darnell-love27@users.noreply.github.com>
2023-03-02 17:15:48 -0500

    Added add, remove, union/intersect.

--- lsp-spring-2023/src/org/howard/edu/hw4/IntegerSet.java
+++ lsp-spring-2023/src/org/howard/edu/hw4/IntegerSet.java
@@ -33,7 +33,63 @@ public class IntegerSet {
 	}
 	//
 	public boolean equals(IntegerSet set) {
-		
+		if(new_set.size() == set.length()) {
+			return new_set.containsAll(set.new_set);
+		}else {
+			return false;
+		}
+	public int large() throws IntegerSetException {
+		int maxVal = Integer.MIN_VALUE;
+		if (new_set.isEmpty()) {
+			throw new IntegerSetException("Set is Empty");
+		}
+		else {
+			for (Integer integer : new_set) {
+				if (integer > maxVal) {
+					maxVal = integer;
+				}
+			}
+		}
+	return maxVal;
 	}
+	public int small() throws IntegerSetException{
+		int minVal = Integer.MAX_VALUE;
+		if(new_set.isEmpty()) {
+			throw new IntegerSetException("Set is Empty");
+		}
+		else {
+			for (Integer integer : new_set) {
+				if(integer < minVal) {
+					minVal = integer;
+				}
+			}
+		}
+		return minVal;
+	}
+	
+	public void add(int num) {
+		if(new_set.contains(num) == false) {
+			new_set.add(num);
+		}
+	}
+	
+	public void remove(int num) {
+		if (new_set.contains(num) == true) {
+			new_set.remove(num);
+		}
+	}
+	
+	public void union(IntegerSet number) {
+		for(int x = 0; x < number.new_set.size(); x++) {
+			if(new_set.contains(number.new_set.get(x)) == false) {
+				new_set.add(number.new_set.get(x));
+			}
+		}
+	
+	}
+	public void intersect(IntegerSet number) {
+		new_set.retainAll(number.new_set);
+	}
+	
 
 }
