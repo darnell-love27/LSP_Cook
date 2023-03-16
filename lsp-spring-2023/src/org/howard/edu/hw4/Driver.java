58c60fc499fb798cc58556198f5d9b03380b4fac
darnell-love27 <101828196+darnell-love27@users.noreply.github.com>
2023-03-16 17:13:25 -0400

    Made changes to code.

--- lsp-spring-2023/src/org/howard/edu/hw4/Driver.java
+++ lsp-spring-2023/src/org/howard/edu/hw4/Driver.java
@@ -1,7 +1,10 @@
 //Name: Darnell Love
 package org.howard.edu.hw4;
-import IntegerSet;
 
+import static org.junit.Assert.assertEquals;
+
+import org.howard.edu.hw4.IntegerSet.IntegerSetException;
+import org.howard.edu.hw5.IntegerSet;
 
 /**
  * 
@@ -10,17 +13,41 @@ import IntegerSet;
 
 public class Driver {
 
-	public static void main(String[] args) {
-		IntegerSet set1 = new IntegerSet();
-		
+	public static void main(String[] args) throws org.howard.edu.hw5.IntegerSet.IntegerSetException {
+		IntegerSet new_set = new IntegerSet();
 		//Tests adding elements to set
-		set1.add(1);
-		set1.add(2);
-		set1.add(3);
+		IntegerSet set2 = new IntegerSet();
+		new_set.add(-1);
+		new_set.add(-2);
+		new_set.add(-3);
+		new_set.add(-4);
 		
-		System.out.println("Value of Set1 is:" + set1.toString());
-		System.out.println("Smallest value in Set1 is:" + set1.smallest());
-
+		set2.add(2);
+		set2.add(4);
+		set2.add(-4);
+		
+		System.out.println("Value of new_set is:" + new_set.toString());
+		System.out.println("Value of set2 is:" + set2.toString());
+		
+		System.out.println("Smallest value in new_set is:" + new_set.smallest());
+		System.out.println("Largest value in new_set is:" + new_set.largest());
+		System.out.println("Smallest value in set2 is:" + set2.smallest());
+		System.out.println("Largest value in set2 is:" + set2.largest());
+		
+		IntegerSet union_set = new IntegerSet();
+		union_set = new_set;
+		union_set.union(set2);
+		System.out.println("Union of new_set and set2 is:" + union_set.toString());
+		
+		IntegerSet intersect_set = new IntegerSet();
+		intersect_set = new_set;
+		intersect_set.intersect(set2);
+		
+		System.out.println(intersect_set.toString());
+		
+		
+		
+		
 	}
 
 }
