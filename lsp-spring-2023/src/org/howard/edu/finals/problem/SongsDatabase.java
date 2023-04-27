63a833d829134d57f0e93544673956a53a78945f
darnell-love27 <101828196+darnell-love27@users.noreply.github.com>
2023-04-27 19:54:11 -0400

    Create getGenreOfSong()

--- lsp-spring-2023/src/org/howard/edu/finals/problem/SongsDatabase.java
+++ lsp-spring-2023/src/org/howard/edu/finals/problem/SongsDatabase.java
@@ -38,5 +38,19 @@ public class SongsDatabase {
 		//Otherwise return HashSet 
 		return current_song;
 	}
+	
+	public String getGenreOfSong(String songTitle) {
+		//Iterate through to find the songs of each genre
+		for (String genre:song_map.keySet()) {
+		//Create new HashSet to access songs from given genre 
+			HashSet<String> new_songs = song_map.get(genre);
+			//If new_songs has the title -> return genre
+			if (new_songs.contains(songTitle)) {
+				return genre;
+			}
+		}
+		//If no songs are in the genre -> return null
+		return null;
+	}
 }
 
