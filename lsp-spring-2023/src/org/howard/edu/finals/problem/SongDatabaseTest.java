f28b1a2193e69a3c0b6ec4e752de094dabccf6b7
darnell-love27 <101828196+darnell-love27@users.noreply.github.com>
2023-04-27 20:27:38 -0400

    Added two more test cases for testAddSong().

--- lsp-spring-2023/src/org/howard/edu/finals/problem/SongDatabaseTest.java
+++ lsp-spring-2023/src/org/howard/edu/finals/problem/SongDatabaseTest.java
@@ -32,6 +32,18 @@ public class SongDatabaseTest {
 		Set<String> new_song = song_base.getSongs("R&B");
 		assertEquals(1, new_song.size());
 		assertTrue(new_song.contains("Good Days"));
+		
+		//Test 2:Adding to already in genre
+		song_base.addSong("R&B", "Flowers");
+		new_song = song_base.getSongs("R&B");
+		assertTrue(new_song.contains("Flowers"));
+		assertTrue(new_song.contains("Good Days"));
+		
+		//Test 3:Adding song to new genre
+		song_base.addSong("Hip Hop", "Ms.Jackson");
+		Set<String> hipsongs = song_base.getSongs("Hip Hop");
+		assertFalse(hipsongs.contains("Good Days"));
+		
 	}
 
 }
