0bc3a84681553d5cecb61a3faf15ac36269e114c
darnell-love27 <101828196+darnell-love27@users.noreply.github.com>
2023-04-27 20:18:29 -0400

    Added first test case to testAddSong().

--- lsp-spring-2023/src/org/howard/edu/finals/problem/SongDatabaseTest.java
+++ lsp-spring-2023/src/org/howard/edu/finals/problem/SongDatabaseTest.java
@@ -1,19 +1,37 @@
 package org.howard.edu.finals.problem;
 
 //Import Statements
+import java.util.Set;
+import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class SongDatabaseTest {
 
-	public static void main(String[] args) {
-		
-
+	private SongsDatabase song_base;
+	
+	@Before
+	public void newSongDatabase() {
+		song_base = new SongsDatabase();
+	}
+	
+	@Test
+	@DisplayName("Test addition of songs")
+	//Test Cases for AddSong()
+	public void testAddSong() {
+		//Test 1:Add a singular song
+		song_base = new SongsDatabase();
+		song_base.addSong("R&B", "Good Days");
+		Set<String> new_song = song_base.getSongs("R&B");
+		assertEquals(1, new_song.size());
+		assertTrue(new_song.contains("Good Days"));
 	}
 
 }
